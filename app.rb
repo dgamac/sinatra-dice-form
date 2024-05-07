43168b4e2e09137ff8c4edc76190fb390b123414
dgamac <165616196+dgamac@users.noreply.github.com>
2024-05-07 03:58:18 +0000

    Dice list working

--- app.rb
+++ app.rb
@@ -5,5 +5,15 @@ get("/") do
   erb(:home)
 end
 get("/process_roll") do
+  @num_dice = params.fetch("dice").to_i
+
+  @num_sides = params.fetch("sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    @rolls.push(rand(1..@num_sides))
+  end
+
   erb(:results)
 end
